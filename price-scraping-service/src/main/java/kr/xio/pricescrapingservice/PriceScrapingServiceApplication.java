265c80c0968f858da748cf7a748429ecd96b3bfd
Park JeongSoo <jsoo_dev@naver.com>
2021-11-18 01:23:33 +0900

    create price-scraping-service

---
+++ price-scraping-service/src/main/java/kr/xio/pricescrapingservice/PriceScrapingServiceApplication.java
@@ -0,0 +1,13 @@
+package kr.xio.pricescrapingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PriceScrapingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PriceScrapingServiceApplication.class, args);
+	}
+
+}
---
+++ price-scraping-service/src/test/java/kr/xio/pricescrapingservice/PriceScrapingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.xio.pricescrapingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PriceScrapingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
