14a14c653d8f9fc8214c7b37602687dd6cd8b087
Park JeongSoo <jsoo_dev@naver.com>
2021-11-18 00:41:31 +0900

    create jwt-authentication-service

---
+++ jwt-authentication-service/src/test/java/kr/xio/jwtauthenticationservice/JwtAuthenticationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.xio.jwtauthenticationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtAuthenticationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
