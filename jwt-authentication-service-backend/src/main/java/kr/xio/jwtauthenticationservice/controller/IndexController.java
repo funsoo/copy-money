8b94b540a73cfa7ff09ccd12a433d2eba71534e8
thsgusdnr123 <thsgusdnr123@naver.com>
2021-12-13 22:03:18 +0900

    Spring boot React 연동 테스트

--- jwt-authentication-service-backend/src/main/java/kr/xio/jwtauthenticationservice/controller/IndexController.java
+++ jwt-authentication-service-backend/src/main/java/kr/xio/jwtauthenticationservice/controller/IndexController.java
@@ -1,14 +1,52 @@
 package kr.xio.jwtauthenticationservice.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "http://localhost:3000")
+@RestController
 @Controller
 public class IndexController {
 
     @RequestMapping(value = "/")
     public String index() {
-        return "index";
+        return "esx";
+    }
+
+    @GetMapping("/user")
+    public String user(){
+
+        return "user";
+    }
+    @GetMapping("/admin")
+    public String admin(){
+
+        return "admin";
+    }
+    @GetMapping("/manager")
+    public String manager(){
+
+        return "manager";
+    }
+    @GetMapping("/login")
+    public String login(){
+
+        return "login";
+    }
+    @GetMapping("/join")
+    public String join(){
+
+        return "join";
+    }
+
+    @GetMapping("/joinProc")
+    public String joinProc(){
+
+        return "회원가입 완료";
     }
     
 }
--- jwt-authentication-service-frontend/src/App.js
+++ jwt-authentication-service-frontend/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import Axios from "axios";
 
 function App() {
+  const [test, setTest] = useState(null);
+
+  const getTest = async () => {
+    let res = await Axios.get("http://localhost:8080/");
+
+    setTest(res.data)
+
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>test : {test}</h1>
+      <button onClick={getTest}>Test</button>
     </div>
   );
 }
